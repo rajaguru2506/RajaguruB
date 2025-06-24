2c977715e4c55521814ceba48b7e9d77e204e876
rajaguru2506 <brguru55555@gmail.com>
2025-06-24 16:41:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let result = eval(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = "Error";
+  }
+}
